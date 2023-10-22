4872e3390a98ffbaad4d8439c7caf8ed0d336676
jduhking <jamesbodebiyi@gmail.com>
2023-10-21 19:59:49 -0500

    Added sending of mp3 files

--- voice-backend/main.py
+++ voice-backend/main.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
@@ -8,8 +9,17 @@ app = FastAPI()
 def read_root():
     print('hit!')
     return { "message" : "Hello world!"}
+@app.get("/audio")
+def send_audio():
+    audio_path = "./shout.mp3"
 
+    return FileResponse(audio_path, media_type="audio/mpeg", filename="shout.mp3")
 @app.post("/uploadaudio")
 async def parse_audio(audio_file: UploadFile):
 
     return {"filename": audio_file.filename}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
